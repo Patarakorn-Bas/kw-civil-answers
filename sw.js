7ec00f8fae87cc4a773d5dd5da426528e17322ce
Patarakorn-Bas <Patarakorn21ce@gmail.com>
2026-09-20 20:53:46 +0700

    first version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='kw-1789908702';
+const V='kw-1789911156';
 const CORE=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
