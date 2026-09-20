2b05e6b9d3549a9185046a5cc551b4f007e7de49
Patarakorn-Bas <Patarakorn21ce@gmail.com>
2026-09-20 20:22:50 +0700

    first version

---
+++ sw.js
@@ -0,0 +1,12 @@
+const V='kw-1789908702';
+const CORE=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  const r=e.request; if(r.method!=='GET'||new URL(r.url).origin!==location.origin) return;
+  e.respondWith(caches.open(V).then(async c=>{
+    const hit=await c.match(r);
+    const net=fetch(r).then(res=>{ if(res&&res.ok) c.put(r,res.clone()); return res; }).catch(()=>hit);
+    return hit||net;
+  }));
+});
